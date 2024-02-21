a7c13067caae1d2fcf7cf60de0f9ac49634ec6b7
DavidAlex99 <daalrive@espol.edu.ec>
2024-02-21 11:05:39 -0500

    reorganizacion de carpetas

--- gates/gatesApp/views.py
+++ gates/gatesApp/views.py
@@ -4,24 +4,24 @@ from django.shortcuts import render, HttpResponse
 
 #
 def home(request):
-    return render(request, "gatesApp/home.html")
+    return render(request, "home.html")
 
 # vista para el menu
 def servicios(request):
-    return render(request, "gatesApp/servicios.html")
+    return render(request, "servicios.html")
 
 # vista para Sobre Nosotros
 def acerca(request):
-    return render(request, "gatesApp/acerca.html")
+    return render(request, "acerca.html")
 
 #vista para el contacto
 def contacto(request):
-    return render(request, "gatesApp/contacto.html")
+    return render(request, "contacto.html")
 
 #vista para la ubicacion
 def ubicacion(request):
-    return render(request, "gatesApp/ubicacion.html")
+    return render(request, "ubicacion.html")
 
 # vista para la galeria
 def calendario(request):
-    return render(request, "gatesApp/calendario.html")
+    return render(request, "calendario.html")
